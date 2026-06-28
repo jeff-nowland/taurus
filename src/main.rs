d18f10090dccd324968c8cf95a076724b1d4eb8f
Jeffrey Nowland <jnowland@comcast.net>
2026-06-28 13:11:04 -0500

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
+use clap::{Command};
+
 pub mod taurus;
 
 fn main() {
-    println!("Hello, world!");
+    let cmd = Command::new("taurus")
+        .bin_name("taurus")
+        .styles(CLAP_STYLE)
+        .subcommand_required(true);
+    let matches = cmd.get_matches();
+    let _matches = match matches.subcommand() {
+        _ => unreachable!("unrecognized subcommand."),
+    };
 }
+
+use clap::builder::styling::{Styles};
+use clap_cargo::style::*;
+
+const CLAP_STYLE: Styles = Styles::styled()
+    .header(HEADER)
+    .usage(USAGE)
+    .literal(LITERAL)
+    .placeholder(PLACEHOLDER)
+    .error(ERROR)
+    .valid(VALID)
+    .invalid(INVALID);
+
+//const SUBCOMMANDS = [clap_command!("help")];
